eb06c5d43531e17db93f30e47085100b372a8a81
angelexi76 <angelasanchezbarbosa@gmail.com>
2022-10-21 23:48:09 -0500

    botones login en Mainactivity

--- app/src/main/java/com/android/appmovilmisiontic/MainActivity.kt
+++ app/src/main/java/com/android/appmovilmisiontic/MainActivity.kt
@@ -1,6 +1,8 @@
 package com.android.appmovilmisiontic
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
+import android.view.View
 import android.os.Bundle
 
 class MainActivity : AppCompatActivity() {
@@ -8,4 +10,23 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
+
+
+
+    fun login1(btnlogin1:View){
+
+        val ingreso1=Intent(this,ingreso_usuario::class.java)
+        startActivity(ingreso1)
+    }
+
+    fun login(view: View) {
+        val ingreso=Intent(this,ingreso_usuario::class.java)
+        startActivity(ingreso)
+    }
+
+
+}
+
+class ingreso_usuario {
+
 }
