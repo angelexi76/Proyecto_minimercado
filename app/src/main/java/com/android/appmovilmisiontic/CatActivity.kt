df8e2132ddfddbb3e05d3bb3852ae12ed545fdd6
angelexi76 <angelasanchezbarbosa@gmail.com>
2022-11-05 12:01:02 -0500

    actividad login y catalogo,diseño

---
+++ app/src/main/java/com/android/appmovilmisiontic/CatActivity.kt
@@ -0,0 +1,10 @@
+package com.android.appmovilmisiontic
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class CatActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+}
---
+++ app/src/main/java/com/android/appmovilmisiontic/LoginActivity.kt
@@ -0,0 +1,10 @@
+package com.android.appmovilmisiontic
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class LoginActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+}
